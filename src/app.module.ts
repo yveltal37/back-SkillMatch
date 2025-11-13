1ddf17bf07dddfcec63bbe0a8b6d74e5dc5d5af5
Yarin <yarin.200610@gmail.com>
2025-11-13 13:09:41 +0200

    feat(cate): Add category module, service, and controller setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { AuthenticationModule } from './authentication/authentication.module';
+import { CategoryModule } from './category/category.module';
 
 @Module({
-  imports: [DatabaseModule, AuthenticationModule, ],
+  imports: [DatabaseModule, AuthenticationModule, CategoryModule, ],
   controllers: [AppController],
   providers: [AppService],
 })
