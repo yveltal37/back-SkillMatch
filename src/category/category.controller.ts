b894fb2081376cc509a7cd2778438132dab3521f
Yarin <yarin.200610@gmail.com>
2025-11-13 18:33:22 +0200

    feat(cate): add CategoryController with create, delete, and getUserCategories secured by JWT

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Delete, Param, Body, ParseIntPipe, UseGuards, Req } from '@nestjs/common';
+import { CategoryService } from './category.service';
+import { JwtAuthGuard } from '../authentication/jwt/jwt-auth.guard';
 
 @Controller('category')
-export class CategoryController {}
+@UseGuards(JwtAuthGuard)
+export class CategoryController {
+  constructor(private readonly categoryService: CategoryService) {}
+
+  @Post()
+  async createCategory(@Body('name') categoryName: string) {
+    return this.categoryService.createCategory(categoryName);
+  }
+
+  @Delete(':id')
+  async deleteCategory(@Param('id', ParseIntPipe) categoryId: number) {
+    return this.categoryService.deleteCategory(categoryId);
+  }
+
+  @Get('my-categories')
+  async getUserCategories(@Req() req) {
+    return this.categoryService.getUserCategories(req.user.id);
+  }
+}
