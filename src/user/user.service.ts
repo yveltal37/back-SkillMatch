e831068d26c6a5d98430d034c19e04a32ec5b6ac
Yarin <yarin.200610@gmail.com>
2025-11-27 00:30:20 +0200

    build getUsers and getUserStatintics methods on UserService

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,38 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  BadRequestException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository, In } from 'typeorm';
+import { User } from '../entities/user.entity';
+import { UserCategory } from '../entities/user_category.entity';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepo: Repository<User>,
+    @InjectRepository(UserCategory)
+    private readonly userCategoryRepo: Repository<UserCategory>,
+  ) {}
+
+  async getAllUsers() {
+    return this.userRepo.find({
+      select: ['id', 'username', 'isAdmin', 'createdAt'],
+    });
+  }
+
+  async getUserStatistics(username: string) {
+    const user = await this.userRepo.findOne({
+      where: { username },
+      relations: ['userCategories'],
+    });
+    if (!user) throw new NotFoundException('User not found');
+
+    return user.userCategories.map((uc) => ({
+      categoryName: uc.category.name,
+      completedChallenges: uc.completedCount,
+    }));
+  }
+}
